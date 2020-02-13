c322d4b1d8e7149bf0effbb8503b3a6241fd4a3f
Salvador <salvador.on@outlook.com>
2020-02-13 20:41:14 -0300

    add count method

--- main.rb
+++ main.rb
@@ -22,43 +22,72 @@ module Enumerable
   def my_select
     varr = []
     iarr = self
-    iarr.my_each do |num|
-      varr << num if yield(num) == true
-    end
+    iarr.my_each { |num| varr << num if yield(num) == true }
     varr
   end
 
   def my_all?
     iarr = self
-    iarr.my_each do |num|
-      return false unless yield(num)
-    end
+    iarr.my_each { |num| return false unless yield(num) }
     true
   end
 
   def my_any?
     iarr = self
-    iarr.my_each do |num|
-      return true unless yield(num)
-    end
+    iarr.my_each { |num| return true unless yield(num) }
     false
+  end
+
+  def my_none?
+    iarr = self
+    iarr.my_each { |num| return false if yield(num) }
+    true
+  end
+
+  def my_count(val = nil)
+    iarr = self
+    size = iarr.length
+    x = 0
+    if !block_given? && val.nil?
+      x = size
+    elsif val && !block_given?
+      iarr.my_each { |num| x += 1 if num == val }
+    elsif block_given? && val.nil?
+      iarr.my_each { |num| x += 1 if yield(num) }
+    end
+    x
   end
 end
 
-arr = [8, 3, 5, 6]
-puts 'my each'
+arr = [8, 3, 5, 5, 6, 6]
+puts '-----my each-----'
 arr.my_each { |num1| puts num1 }
-puts 'my each with index'
+puts '-----my each with index-----'
 arr.my_each_with_index { |num1, num2| p num1.to_s + ':' + num2.to_s }
-puts 'my select--------------?'
+puts '-----my select?-----'
 p(arr.my_select { |num| num > 4 })
-puts 'my all--------------?'
+puts '-----my all?--------'
 res = arr.all? { |num| num > 2 }
 puts res
 res = arr.all? { |num| num > 6 }
 puts res
-puts 'my any--------------?'
+puts '-----my any--------'
 res = arr.any? { |num| num > 5 }
 puts res
 res = arr.any? { |num| num > 9 }
 puts res
+puts '-----my none?--------'
+res = arr.none? { |num| num > 5 }
+puts res
+res = arr.none? { |num| num > 9 }
+puts res
+puts '-----count--------'
+puts arr.count
+puts arr.count(6)
+puts(arr.count { |num| num > 4 })
+puts '-----my count?--------'
+res = arr.my_count
+puts res
+res = arr.my_count(6)
+puts res
+puts(arr.my_count { |num| num > 4 })
